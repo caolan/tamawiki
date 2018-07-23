3585ed69dcbffde6a9ce8a845a49513375bd0f9e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-23 09:18:53 +0000

    Deploy caolan/tamawiki to github.com/caolan/tamawiki.git:gh-pages

---
+++ actix/actors/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ConnectorError",""]],"mod":[["mocker","Mocking utility actor This actor wraps any actor, and replaces instances of that actor with mocker actor, which is able to accept all messages which the actor can receive."],["signal","An actor implementation of Unix signal handling"]],"struct":[["Connect",""],["ConnectAddr",""],["Connector",""],["Resolve",""]]});
---
+++ implementors/core/fmt/trait.UpperHex.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["uuid"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"uuid/prelude/struct.Uuid.html\" title=\"struct uuid::prelude::Uuid\">Uuid</a>",synthetic:false,types:["uuid::Uuid"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"uuid/struct.Simple.html\" title=\"struct uuid::Simple\">Simple</a>&lt;'a&gt;",synthetic:false,types:["uuid::Simple"]},{text:"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.UpperHex.html\" title=\"trait core::fmt::UpperHex\">UpperHex</a> for <a class=\"struct\" href=\"uuid/struct.Hyphenated.html\" title=\"struct uuid::Hyphenated\">Hyphenated</a>&lt;'a&gt;",synthetic:false,types:["uuid::Hyphenated"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
