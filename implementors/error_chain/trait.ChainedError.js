28d76eeee19e3d08caec5635a664487a7f9a0bd2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-27 08:09:33 +0000

    Deploy caolan/tamawiki to github.com/caolan/tamawiki.git:gh-pages

---
+++ implementors/error_chain/trait.ChainedError.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["error_chain"] = [];
+implementors["tera"] = [{text:"impl <a class=\"trait\" href=\"error_chain/trait.ChainedError.html\" title=\"trait error_chain::ChainedError\">ChainedError</a> for <a class=\"struct\" href=\"tera/struct.Error.html\" title=\"struct tera::Error\">Error</a>",synthetic:false,types:["tera::errors::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ tamawiki/templates/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["render_response",""]]});
