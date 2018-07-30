14f244f804e015728e1334bb2c27eccd5ec3415f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-07-30 19:22:34 +0000

    Deploy caolan/tamawiki to github.com/caolan/tamawiki.git:gh-pages

--- aliases.js
+++ aliases.js
@@ -171,6 +171,7 @@ ALIASES['tamawiki'] = {};
 ALIASES['tamawiki'] = {};
 ALIASES['tamawiki'] = {};
 ALIASES['tamawiki'] = {};
+ALIASES['tamawiki'] = {};
 ALIASES['tera'] = {};
 ALIASES['tera'] = {};
 ALIASES['thread_local'] = {};
