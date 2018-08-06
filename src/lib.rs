626cfc3855f15824ff594eb6d0e23cc7fbe5c3e1
Caolan McMahon <caolan.mcmahon@gmail.com>
2018-08-06 05:56:56 +0100

    Render pages using Tera templates

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,10 @@
 #[cfg(test)]
 #[macro_use] extern crate proptest;
 
+#[macro_use] extern crate tera;
+#[macro_use] extern crate lazy_static;
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
 extern crate serde;
 extern crate futures;
 extern crate warp;
@@ -17,13 +20,23 @@ pub mod store;
 
 use warp::{Filter, Reply};
 use warp::filters::BoxedFilter;
+use warp::reject::Rejection;
 use futures::future::Future;
 use std::sync::{Arc, Mutex};
 use std::path::PathBuf;
 use http::{StatusCode, Response};
+use serde::Serialize;
+use tera::Tera;
 
 use store::{Store, StoreClient, StoreError, SequenceId};
 use document::Document;
+
+
+lazy_static! {
+    static ref TERA: Tera = {
+        compile_templates!("templates/**/*")
+    };
+}
 
 
 pub fn app<T: 'static + Store>(store: Arc<Mutex<T>>) -> BoxedFilter<(impl Reply,)> {
@@ -38,29 +51,31 @@ pub fn app<T: 'static + Store>(store: Arc<Mutex<T>>) -> BoxedFilter<(impl Reply,
         .map(|tail: warp::path::Tail| PathBuf::from(tail.as_str()));
 
     // get document content from store or return error
-    let document_content = warp::any()
+    let display_document = warp::any()
         .and(store_client)
         .and(document_path)
         .and_then(|store: T::Client, tail: PathBuf| {
-            store.content(&tail.as_path())
-                .map_err(|err| {
-                    match err {
-                        StoreError::NotFound => warp::reject::not_found(),
-                        _ => warp::reject::server_error(),
-                    }
-                })
-        });
-
-    // display document on success
-    let display_document = warp::get(
-        document_content
-            .map(|data: (SequenceId, Document)| {
-                Response::builder()
-                    .status(StatusCode::OK)
-                    .header("Content-Type", "text/html")
-                    .body(format!("{}\n\n\nseq: {}", data.1.content, data.0))
+            store.content(&tail.as_path()).map_err(|err| {
+                match err {
+                    StoreError::NotFound => warp::reject::not_found(),
+                    _ => warp::reject::server_error()
+                }
             })
-    );
+        })
+        .and_then(|(seq, doc): (SequenceId, Document)| {
+            template("document.html", &json!({
+                "title": "Document",
+                "seq": seq,
+                "content": doc.content
+            }))
+        })
+        .map(|body| {
+            Response::builder()
+                .status(StatusCode::OK)
+                .header("Content-Type", "text/html")
+                .body(body)
+                .unwrap()
+        });
 
     let static_files = warp::path("_static")
         .and(warp::fs::dir("static"));
@@ -68,16 +83,36 @@ pub fn app<T: 'static + Store>(store: Arc<Mutex<T>>) -> BoxedFilter<(impl Reply,
     // app filter
     warp::get(static_files)
         .or(display_document
-            .or_else(|err: warp::reject::Rejection| {
-                match err.status() {
-                    StatusCode::NOT_FOUND => {
-                        Ok((Response::builder()
-                            .status(StatusCode::NOT_FOUND)
-                            .header("Content-Type", "text/html")
-                            .body("Not found\n".to_owned()),))
-                    },
-                    _ => Err(err)
-                }
-            }))
+            .or_else(render_rejection))
         .boxed()
+}
+
+
+fn template<T: Serialize>(name: &'static str, context: &T) ->
+    Result<String, warp::Rejection>
+{
+    TERA.render(name, &context).map_err(|_| {
+        warp::reject::server_error()
+    })
+}
+
+fn render_rejection(err: Rejection) ->
+    Result<(Response<String>,), Rejection>
+{
+    match err.status() {
+        StatusCode::NOT_FOUND => {
+            Ok((
+                Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .header("Content-Type", "text/html")
+                    .body(
+                        template("404.html", &json!({
+                            "title": "Not found"
+                        }))?
+                    )
+                    .unwrap(),
+            ))
+        },
+        _ => Err(err),
+    }
 }
