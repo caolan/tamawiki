2c25c29a55974159d4bd68ecc983f3e6d689de07
Caolan McMahon <caolan.mcmahon@gmail.com>
2018-08-09 08:50:19 +0100

    Render pages using Tera templates

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@
 #[cfg(test)]
 #[macro_use] extern crate proptest;
 
+#[macro_use] extern crate tera;
+#[macro_use] extern crate lazy_static;
 #[macro_use] extern crate serde_derive;
+#[macro_use] extern crate serde_json;
 extern crate serde;
 extern crate hyper;
 extern crate futures;
--- src/service.rs
+++ src/service.rs
@@ -25,12 +25,19 @@ use hyper::body::Body;
 use hyper::service::{NewService, Service};
 use futures::future::{self, Future, FutureResult};
 use http::StatusCode;
+use tera::Tera;
+use std::fmt::{self, Display};
 use std::error::Error;
 use std::path::PathBuf;
-use std::fmt;
 
 use store::{Store, StoreError};
 
+
+lazy_static! {
+    static ref TERA: Tera = {
+        compile_templates!("templates/**/*")
+    };
+}
 
 /// Error conditions that could not be handled as a HTTP response
 #[derive(Debug)]
@@ -74,41 +81,93 @@ pub struct TamaWikiService<T: Store> {
     pub store: T
 }
 
-impl<T: Store> Service for TamaWikiService<T> {
-    type ReqBody = Body;
-    type ResBody = Body;
-    type Error = TamaWikiError;
-    type Future = Box<Future<Item=Response<Self::ResBody>, Error=Self::Error> + Send>;
-    
-    fn call(&mut self, req: Request<Self::ReqBody>) -> Self::Future {
+#[derive(Debug)]
+enum HttpError {
+    InternalServerError(String)
+}
+
+impl Display for HttpError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            HttpError::InternalServerError(ref err) => 
+                write!(f, "InternalServerError: {}", err),
+        }
+    }
+}
+
+impl Error for HttpError {
+    fn description(&self) -> &str {
+        match *self {
+            HttpError::InternalServerError(ref err) => err,
+        }
+    }
+}
+
+impl<T: Store> TamaWikiService<T> {
+    fn serve_document(&mut self, req: &Request<Body>) ->
+        Box<Future<Item=Response<Body>, Error=HttpError> + Send>
+    {
         let path = PathBuf::from(&req.uri().path()[1..]);
         Box::new(
             self.store.content(&path.as_path()).then(|result| {
                 match result {
-                    Ok((_seq, doc)) => {
-                        future::ok(
-                            Response::builder()
-                                .body(Body::from(doc.content))
-                                .unwrap()
-                        )
+                    Ok((seq, doc)) => {
+                        let ctx = json!({
+                            "title": "Document",
+                            "content": doc.content,
+                            "seq": seq
+                        });
+                        let text = TERA.render("document.html", &ctx).unwrap();
+                        Ok(Response::builder()
+                           .body(Body::from(text))
+                           .unwrap())
                     }
                     Err(StoreError::NotFound) => {
-                        future::ok(
-                            Response::builder()
-                                .status(StatusCode::NOT_FOUND)
-                                .body(Body::from("Not found"))
-                                .unwrap()
-                        )
+                        let ctx = json!({
+                            "title": "Document",
+                        });
+                        let text = TERA.render("empty_document.html", &ctx).unwrap();
+                        Ok(Response::builder()
+                           .status(StatusCode::NOT_FOUND)
+                           .body(Body::from(text))
+                           .unwrap())
                     },
-                    Err(_) => {
-                        future::ok(
-                            Response::builder()
-                                .status(StatusCode::INTERNAL_SERVER_ERROR)
-                                .body(Body::from("Internal server error"))
-                                .unwrap()
-                        )
+                    Err(err) => {
+                        Err(HttpError::InternalServerError(format!("{}", err)))
                     }
                 }
+            })
+        )
+    }
+}
+
+impl<T: Store> Service for TamaWikiService<T> {
+    type ReqBody = Body;
+    type ResBody = Body;
+    type Error = TamaWikiError;
+    type Future = Box<Future<Item=Response<Self::ResBody>,
+                             Error=Self::Error> +
+                      Send>;
+    
+    fn call(&mut self, req: Request<Self::ReqBody>) -> Self::Future {
+        Box::new(
+            self.serve_document(&req).then(|result| {
+                match result {
+                    Ok(response) => future::ok(response),
+                    Err(HttpError::InternalServerError(err)) => {
+                        let ctx = json!({
+                            "title": "Internal Server Error",
+                            "error": err
+                        });
+                        let text = TERA.render("500.html", &ctx).unwrap();
+                        future::ok(Response::builder()
+                                   .status(StatusCode::INTERNAL_SERVER_ERROR)
+                                   .body(Body::from(text))
+                                   .unwrap())
+                    }
+                }
+            }).map_err(|_err: HttpError| {
+                TamaWikiError {}
             })
         )
     }
